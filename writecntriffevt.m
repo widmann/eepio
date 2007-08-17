fdcf81b7b290a168ebfde8d40f6ed2541d698fcd
widmann <widmann@uni-leipzig.de>
2007-08-17 11:59:27 +0000

    Convert boundary events

--- writecntriffevt.m
+++ writecntriffevt.m
@@ -1,6 +1,6 @@
 % $Id$
 
-function writeriffevt(fid, Event)
+function writecntriffevt(fid, Event)
 
 if nargin < 2
     error('Not enough input arguments.')
@@ -9,14 +9,18 @@ end
 % Chunk header
 chunkOffset = writeriffchunk(fid, 'evt');
 
+% Convert boundary to __
+[Event(strmatch('boundary', {Event.type})).type] = deal('__');
+
 for iEvt = 1:length(Event)
 
     % Write event offset
-    fwrite(fid, Event(iEvt).latency - 1, 'uint32');
+    fwrite(fid, ceil(Event(iEvt).latency) - 1, 'uint32');
     
     % Format and write event type
-    evtTypeArray = num2str(strzeropad(Event(iEvt).type, 8));
+    evtTypeArray = num2str(zeropad(Event(iEvt).type, 8, 2));
     fwrite(fid, evtTypeArray(1:8), 'char');
+
 end
 
 % Write chunk size and word align file pointer
